6e3e23dbf350439a6df8a8141c76f8a1ceda64a2
Velmurugan Balasubramanian <velmurugan1124@gmail.com>
2019-04-25 07:11:30 +0530

    working current and forecast weather

---
+++ app/app-config.js
@@ -0,0 +1,7 @@
+((module) => {
+    const CITY_NAMES = ["Amsterdam","Bangkok","Beijing","Barcelona","Bengaluru","Chennai","Delhi","Mumbai","Melbourne","Munich","Paris","Sydney"];
+
+    module.getDefaultCityList =  function() {
+        return CITY_NAMES;
+    }
+})(window.fdWeatherApp = window.fdWeatherApp || {});
--- app/app.js
+++ app/app.js
@@ -3,62 +3,141 @@
     app.initialized()
       .then(function (_client) {
         var client = _client;
+        const current_url = "https://api.openweathermap.org/data/2.5/weather?"
+        const forecast_url = "https://api.openweathermap.org/data/2.5/forecast?";
+        const toCelsius = 273.15;
+        var forecastArray = [];
+        
         client.events.on('app.activated',
           function () {
-            client.data.get('contact')
-              .then(function (data) {
-                $('#apptext').text("Ticket created by " + data.contact.name);
-                //  var api = "<%= iparam.api %> Hello there" ;
-                //console.log("API",api);
 
-              })
+            client.iparams.get("api_key")
+              .then(function (data) {
+                const api_key = data.api_key;
+
+                getCities();
+                getLastFiveCities();
+
+                getCurrentWeather(api_key)
+                $("#find-weather").click(() => {
+                  getWeatherForecast(api_key)
+                });
+
+
+              },
+                function (error) {
+                  console.log(error);
+                  // failure operation
+                })
               .catch(function (e) {
                 console.log('Exception - ', e);
               });
 
-            // var options = {body: JSON.stringify({
-            //     q:"London",
-            //     //appid: "<%= iparam.api_key %>"
-            //     appid:"17f91e9f0a025a4e616b50c063eb7876"
-            // })};
-            //var options = {};
-            var url = "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=";
-            // client.request.post(url, options)
-            //   .then(
-            //     function (data) {
-            //       console.log("posstttt", data);
-            //     },
-            //     function (error) {
-            //       console.log(error);
-            //     });
 
-            client.iparams.get("api_key").then(
-              function (data) {
-                console.log("APIKEY", data);
-                getWeatherData(data.api_key)
-              },
-              function (error) {
-                console.log(error);
-                // failure operation
-              }
-            );
+            function getCurrentWeather(api_key) {
+              client.data.get('contact')
+                .then(function (data) {
+                  city = data.contact.time_zone
+                  $('#current-city').text(city);
+                  client.request.post(`${current_url}q=${city}&appid=${api_key}`)
+                    .then(
+                      function (data) {
+                        console.log(JSON.parse(data.response));
+                        const weatherInCelsius = JSON.parse(data.response).main.temp - toCelsius;
+                        console.log(JSON.parse(data.response));
+
+                        const windSpeed = JSON.parse(data.response).wind.speed
+                        $('#current-temp').text(weatherInCelsius);
+                        $('#current-windspeed').text(windSpeed);
+                      },
+                      function (error) {
+                        console.log(error);
+                      });
 
 
-            function getWeatherData(api_key) {
-              console.log("apikeu",api_key);
-              console.log("full URL","https://api.openweathermap.org/data/2.5/forecast?q=London&appid="+api_key);
-              client.request.post(url+api_key)
-                .then(
-                  function (data) {
-                    console.log("posstttt12334455", data.response);
+                })
+                .catch(function (e) {
+                  console.log('Exception - ', e);
+                });
+            }
+            function getWeatherForecast(api_key) {
+
+              const choosenDate = $('#weather-date').val();
+              const choosenCity = $('#selected-city').val();
+
+              client.request.post(`${forecast_url}q=${choosenCity}&appid=${api_key}`)
+                .then(function (data) {
+                  const resultArray = JSON.parse(data.response).list;
+                  var forecast = "";
+                  var forecastWeather = "";
+                  var forecastWindSpeed = ""
+                  resultArray.forEach(function (data) {
+                    if (data.dt_txt == choosenDate + " 06:00:00") {
+                      forecast = data;
+                      forecastWeather = forecast.main.temp;
+                      forecastWeather = forecastWeather - toCelsius;
+                      forecastWeather = forecastWeather.toFixed(0);
+                      forecastWindSpeed = forecast.wind.speed;
+                    }
+                  })
+
+                  $('#forecast').empty();
+                  $('#forecast').append(`
+                  <h3>Weather Forecast at ${choosenCity} </h3>
+                  <div class="fw-content-list">
+                    <div class="muted"> Temperature</div>
+                    <div> ${forecastWeather}</div>
+                  </div>
+                  <div class="fw-content-list">
+                    <div class="muted">Forecast wind Speed</div>
+                    <div>${forecastWindSpeed}</div>
+                  </div>`);
+
+                  storeLastFiveCities(choosenCity);
+
                   },
                   function (error) {
                     console.log(error);
-                  });
-
+                  })
+                .catch(function (e) {
+                  console.log('Exception - ', e);
+                });
             }
 
+            function getCities(){
+            const cityList = fdWeatherApp.getDefaultCityList();
+            cityList.forEach(function (data) {
+             console.log(data);
+             $('#selected-city').append(`<option value="${data}">${data}</option>`) ;
+            })
+            console.log("All cities",fdWeatherApp.getDefaultCityList());
+            }
 
+            function storeLastFiveCities(forecast){
+              //var forecastArray = [];
+              console.log("forecastArray",forecastArray);
+              var storedData = localStorage.getItem("forecast");
+              //console.log("storedData",storedData);
+              if(storedData == null){
+                localStorage.setItem("forecast",JSON.stringify(forecastArray) );
+                console.log("heteeee");
+              }
+              forecastArray = JSON.parse(localStorage.getItem("forecast"));
+              
+              console.log("forecastArray",forecastArray);
+              
+             forecastArray.push(forecast)
+              console.log("forecastArray",forecastArray);
+              //localStorage.setItem("forecast", forecastArray);
+              localStorage.forecast = forecastArray;
+              getLastFiveCities();
+            }
+
+            function getLastFiveCities(){
+             
+             console.log("local storage City",localStorage.getItem("forecast"));
+             localStorage.removeItem("forecast");
+            }
           });
       });
   });
